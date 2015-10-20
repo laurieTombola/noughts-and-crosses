c9f685ef0c47e5f4af416e1c8ebb5b8c1fa09f0f
Laurie Athey <laurie.athey@tombola.co.uk>
2015-10-20 14:05:20 +0100

    initial commit

--- main-app/tests/lobby-controller.test.js
+++ main-app/tests/lobby-controller.test.js
@@ -1,4 +1,30 @@
 (function () {
     'use strict';
-    /*TODO: Write some codez*/
+
+    describe('lobby-controller tests', function(){
+        var $scope, controller;
+
+        beforeEach(function(){
+            module('Tombola.Games.NoughtsAndCrosses.Lobby');
+
+            inject(['$rootScope', '$controller', function($rootScope, $controller){
+                $scope = $rootScope.$new();
+                controller = $controller('Lobby', {
+                    $scope:$scope,
+                    $state:mocks.$state,
+                    GameServerProxy:mocks.GameServerProxy,
+                    PlayerType:mocks.PlayerType,
+                    GameStatus:mocks.GameStatus
+                })
+            }]);
+        });
+
+        afterEach(function(){
+
+        });
+
+        it('some text', function(){
+
+        });
+    });
 })();
