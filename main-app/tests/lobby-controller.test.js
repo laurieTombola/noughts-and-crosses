90a83e2baa94eb0548a20ac80c3e35cc55193b28
Laurie Athey <laurie.athey@tombola.co.uk>
2015-10-20 17:23:55 +0100

    written 3 tests for the lobby controller

--- main-app/tests/lobby-controller.test.js
+++ main-app/tests/lobby-controller.test.js
@@ -2,13 +2,15 @@
     'use strict';
 
     describe('lobby-controller tests', function(){
-        var $scope, controller;
+        var $scope, $rootScope, controller, mySpy, mySpy2, mySpy3, $q;
 
         beforeEach(function(){
             module('Tombola.Games.NoughtsAndCrosses.Lobby');
 
-            inject(['$rootScope', '$controller', function($rootScope, $controller){
+            inject(['$rootScope', '$controller', '$q', function(_$rootScope_, $controller, _$q_){
+                $rootScope = _$rootScope_;
                 $scope = $rootScope.$new();
+                $q = _$q_;
                 controller = $controller('Lobby', {
                     $scope:$scope,
                     $state:mocks.$state,
@@ -20,11 +22,39 @@
         });
 
         afterEach(function(){
-
+            mySpy.restore();
         });
 
-        it('some text', function(){
+        it('should call playerType.getPlayer1', function(){
+            mySpy = sinon.spy(mocks.PlayerType, 'getPlayer1');
+            $scope.player1();
+            mySpy.should.have.been.calledOnce;
+        });
 
+        it('should call playerType.getPlayer2', function(){
+            mySpy = sinon.spy(mocks.PlayerType, 'getPlayer2');
+            $scope.player2();
+            mySpy.should.have.been.calledOnce;
+        });
+
+        it('showTheGame should safely launch the game', function(){
+            var deferred = $q.defer();
+            mySpy = sinon.stub(mocks.PlayerType, 'validatePlayerType', function(){return true;});
+            mySpy2 = sinon.spy(mocks.$state, 'go');
+            sinon.stub(mocks.PlayerType, 'getPlayer1', function(){return {type:'human'};});
+            sinon.stub(mocks.PlayerType, 'getPlayer2', function(){return {type:'human'};});
+            sinon.stub(mocks.GameServerProxy, 'APICall', function(){return deferred.promise;});
+
+            $scope.showTheGame();
+
+            deferred.resolve({
+                    data:{
+                        gameboard:'000000000',
+                        outcome:'continue'
+            }});
+            $rootScope.$digest();
+            mySpy.should.have.been.calledTwice;
+            mySpy2.should.have.been.calledOnce.calledWithExactly('game');
         });
     });
 })();
